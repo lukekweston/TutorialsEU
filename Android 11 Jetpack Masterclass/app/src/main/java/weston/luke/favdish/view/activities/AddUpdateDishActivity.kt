45b82e62054fbc718674a919111b4260175b81c9
lkw19 <luke@weston.net.nz>
2023-01-30 12:40:07 +1300

    added onclick listener to the photoicon 0n addUpdateDish

--- Android 11 Jetpack Masterclass/app/src/main/java/weston/luke/favdish/view/activities/AddUpdateDishActivity.kt
+++ Android 11 Jetpack Masterclass/app/src/main/java/weston/luke/favdish/view/activities/AddUpdateDishActivity.kt
@@ -2,11 +2,13 @@ package weston.luke.favdish.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import weston.luke.favdish.R
 import weston.luke.favdish.databinding.ActivityAddUpdateDishBinding
 import weston.luke.favdish.databinding.FragmentAllDishesBinding
 
-class AddUpdateDishActivity : AppCompatActivity() {
+class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var mBinding: ActivityAddUpdateDishBinding
 
@@ -15,15 +17,31 @@ class AddUpdateDishActivity : AppCompatActivity() {
         mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
         setUpActionBar()
+        //Sets the onclick listener to the onClickListener set up in this class
+        mBinding.ivAddDishImage.setOnClickListener(this)
     }
 
 
-    private fun setUpActionBar(){
+    private fun setUpActionBar() {
         setSupportActionBar(mBinding.toolbarAddDishActivity)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         mBinding.toolbarAddDishActivity.setNavigationOnClickListener {
             onBackPressed()
         }
-
     }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.iv_add_dish_image -> {
+                    Toast.makeText(
+                        this, "You have clicked on the imageView",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    return
+                }
+            }
+        }
+    }
+
 }
