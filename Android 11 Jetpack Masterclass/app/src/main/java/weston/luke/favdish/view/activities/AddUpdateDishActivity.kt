d5e91b761585d785545c80db4186a5e5011eebcb
lkw19 <luke@weston.net.nz>
2023-01-30 12:28:54 +1300

    Added a back button to the addUpdateDish activity

--- Android 11 Jetpack Masterclass/app/src/main/java/weston/luke/favdish/view/activities/AddUpdateDishActivity.kt
+++ Android 11 Jetpack Masterclass/app/src/main/java/weston/luke/favdish/view/activities/AddUpdateDishActivity.kt
@@ -3,10 +3,27 @@ package weston.luke.favdish.view.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import weston.luke.favdish.R
+import weston.luke.favdish.databinding.ActivityAddUpdateDishBinding
+import weston.luke.favdish.databinding.FragmentAllDishesBinding
 
 class AddUpdateDishActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityAddUpdateDishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        setUpActionBar()
+    }
+
+
+    private fun setUpActionBar(){
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
     }
 }
