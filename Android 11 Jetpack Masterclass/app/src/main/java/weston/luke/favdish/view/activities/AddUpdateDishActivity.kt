5434d1200db5329316d496cbcbe464ca30808e20
lkw19 <luke@weston.net.nz>
2023-01-30 16:45:09 +1300

    Created a layout for the select image dialog, and wired it up to the activity

--- Android 11 Jetpack Masterclass/app/src/main/java/weston/luke/favdish/view/activities/AddUpdateDishActivity.kt
+++ Android 11 Jetpack Masterclass/app/src/main/java/weston/luke/favdish/view/activities/AddUpdateDishActivity.kt
@@ -1,12 +1,13 @@
 package weston.luke.favdish.view.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import weston.luke.favdish.R
 import weston.luke.favdish.databinding.ActivityAddUpdateDishBinding
-import weston.luke.favdish.databinding.FragmentAllDishesBinding
+import weston.luke.favdish.databinding.DialogCustomImageSelectionBinding
 
 class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -34,14 +35,39 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
         if (v != null) {
             when (v.id) {
                 R.id.iv_add_dish_image -> {
-                    Toast.makeText(
-                        this, "You have clicked on the imageView",
-                        Toast.LENGTH_LONG
-                    ).show()
+                    customImageSelectionDialog()
                     return
                 }
             }
         }
+    }
+
+
+    private fun customImageSelectionDialog(){
+        var dialog = Dialog(this)
+        //    Create a new view binding to the dialog
+        val binding: DialogCustomImageSelectionBinding =
+            DialogCustomImageSelectionBinding.inflate(layoutInflater)
+        dialog.setContentView(binding.root)
+
+
+        binding.tvCamera.setOnClickListener{
+            Toast.makeText(this, "Camera clicked", Toast.LENGTH_LONG).show()
+            //Close the dialog after clicking it
+            dialog.dismiss()
+        }
+
+        binding.tvGallery.setOnClickListener{
+            Toast.makeText(this, "Gallery clicked", Toast.LENGTH_LONG).show()
+            //Close the dialog after clicking it
+            dialog.dismiss()
+        }
+
+
+//        make the dialog appear
+        dialog.show()
+
+
     }
 
 }
