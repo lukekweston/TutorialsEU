b3a568a47daab60b26fd999f7f0136dc2eac760c
lkw19 <luke@weston.net.nz>
2023-01-31 23:22:53 +1300

    Set up constants, created a layout for setting the selectable dish items and created an adapter for it

---
+++ Android 11 Jetpack Masterclass/app/src/main/java/weston/luke/favdish/view/adapters/CustomListItemAdapter.kt
@@ -0,0 +1,36 @@
+package weston.luke.favdish.view.adapters
+
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import weston.luke.favdish.databinding.ItemCustomListBinding
+
+class CustomListItemAdapter(
+    private val activity: Activity,
+    private val listItems: List<String>,
+    private val selection: String
+) : RecyclerView.Adapter<CustomListItemAdapter.ViewHolder>(){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding: ItemCustomListBinding = ItemCustomListBinding.inflate(LayoutInflater.from(activity), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = listItems[position]
+        holder.tvText.text = item
+    }
+
+    override fun getItemCount(): Int {
+        return listItems.size
+    }
+
+
+    class ViewHolder(view: ItemCustomListBinding) : RecyclerView.ViewHolder(view.root){
+        val tvText = view.tvText
+    }
+
+
+}
