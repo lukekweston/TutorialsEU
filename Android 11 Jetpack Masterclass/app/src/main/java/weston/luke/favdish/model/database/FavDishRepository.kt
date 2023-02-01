386de874fa9cd9a4571adae569c692d7198bdd0d
lkw19 <luke@weston.net.nz>
2023-02-01 23:54:20 +1300

    Added a repository

---
+++ Android 11 Jetpack Masterclass/app/src/main/java/weston/luke/favdish/model/database/FavDishRepository.kt
@@ -0,0 +1,12 @@
+package weston.luke.favdish.model.database
+
+import androidx.annotation.WorkerThread
+import weston.luke.favdish.model.entities.FavDish
+
+class FavDishRepository(private val favDishDao: FavDishDao) {
+
+    @WorkerThread
+    suspend fun  insertFavDishData(favDish: FavDish){
+        favDishDao.insertFavDishDetails(favDish)
+    }
+}
