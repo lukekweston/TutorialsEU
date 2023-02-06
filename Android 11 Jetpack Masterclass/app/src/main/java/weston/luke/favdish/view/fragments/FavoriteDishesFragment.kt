95884e77075a04012e733641675e25e3d9854235
lkw19 <luke@weston.net.nz>
2023-02-07 00:07:09 +1300

    refactored code and go the favorite and random dishes displaying correctly

--- Android 11 Jetpack Masterclass/app/src/main/java/weston/luke/favdish/view/fragments/FavoriteDishesFragment.kt
+++ Android 11 Jetpack Masterclass/app/src/main/java/weston/luke/favdish/view/fragments/FavoriteDishesFragment.kt
@@ -7,12 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import weston.luke.favdish.databinding.FragmentDashboardBinding
+import weston.luke.favdish.databinding.FragmentFavouriteDishesBinding
 import weston.luke.favdish.viewmodel.DashboardViewModel
 
-class DashboardFragment : Fragment() {
+class FavoriteDishesFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentFavouriteDishesBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -26,7 +26,7 @@ class DashboardFragment : Fragment() {
         val dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentFavouriteDishesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
--- Android 11 Jetpack Masterclass/app/src/main/java/weston/luke/favdish/view/fragments/RandomDishesFragment.kt
+++ Android 11 Jetpack Masterclass/app/src/main/java/weston/luke/favdish/view/fragments/RandomDishesFragment.kt
@@ -7,12 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import weston.luke.favdish.databinding.FragmentNotificationsBinding
+import weston.luke.favdish.databinding.FragmentRandomDishBinding
 import weston.luke.favdish.viewmodel.NotificationsViewModel
 
-class NotificationsFragment : Fragment() {
+class RandomDishesFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentRandomDishBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -26,7 +26,7 @@ class NotificationsFragment : Fragment() {
         val notificationsViewModel =
             ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentRandomDishBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
