d12f682dce9b8c48693902f367f5f76dbd840718
lkw19 <luke@weston.net.nz>
2023-02-08 00:31:37 +1300

    Created fragment for showing the selected dish info and populated it with the passed in dish

--- Android 11 Jetpack Masterclass/app/src/main/java/weston/luke/favdish/view/fragments/DishDetailsFragment.kt
+++ Android 11 Jetpack Masterclass/app/src/main/java/weston/luke/favdish/view/fragments/DishDetailsFragment.kt
@@ -7,11 +7,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import weston.luke.favdish.R
+import weston.luke.favdish.databinding.FragmentDishDetailsBinding
+import weston.luke.favdish.databinding.FragmentFavouriteDishesBinding
 import weston.luke.favdish.model.entities.FavDish
+import java.io.IOException
+import java.util.*
 
 
 class DishDetailsFragment : Fragment() {
+
+    private var mBinding: FragmentDishDetailsBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -20,15 +28,46 @@ class DishDetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        mBinding = FragmentDishDetailsBinding.inflate(inflater, container, false)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dish_details, container, false)
+        return mBinding!!.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         val args: DishDetailsFragmentArgs by navArgs()
-        Log.i("Dish Title", args.dishDetails.title)
-        Log.i("Dish Title", args.dishDetails.type)
+
+//        Populate the fragment
+        args.let{
+            try{
+                Glide.with(requireActivity())
+                    .load(it.dishDetails.image)
+                    .centerCrop()
+                    .into(mBinding!!.ivDishImage)
+            } catch (e: IOException){
+                e.printStackTrace()
+            }
+
+            mBinding!!.tvTitle.text = it.dishDetails.title
+//            Make first letter uppercase
+            mBinding!!.tvType.text = it.dishDetails.type.replaceFirstChar {
+                if (it.isLowerCase()) it.titlecase(
+                    Locale.ROOT
+                ) else it.toString()
+            }
+            mBinding!!.tvCategory.text = it.dishDetails.category
+            mBinding!!.tvIngredients.text = it.dishDetails.ingredients
+            mBinding!!.tvCookingDirection.text = it.dishDetails.directionsToCook
+            mBinding!!.tvCookingTime.text = resources.getString(R.string.lbl_estimate_cooking_time, it.dishDetails.cookingTime)
+
+
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding = null
     }
 }
