ea46d4eeb8161db5cf0d8f0d3a3fa6549ba2ebff
lkw19 <luke@weston.net.nz>
2023-02-08 22:53:21 +1300

    V29 added palette library and made the background of the dishes based off the color of the image of the dished

--- Android 11 Jetpack Masterclass/app/src/main/java/weston/luke/favdish/view/fragments/DishDetailsFragment.kt
+++ Android 11 Jetpack Masterclass/app/src/main/java/weston/luke/favdish/view/fragments/DishDetailsFragment.kt
@@ -1,13 +1,20 @@
 package weston.luke.favdish.view.fragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.toBitmap
 import androidx.navigation.fragment.navArgs
+import androidx.palette.graphics.Palette
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import weston.luke.favdish.R
 import weston.luke.favdish.databinding.FragmentDishDetailsBinding
 import weston.luke.favdish.databinding.FragmentFavouriteDishesBinding
@@ -39,13 +46,44 @@ class DishDetailsFragment : Fragment() {
         val args: DishDetailsFragmentArgs by navArgs()
 
 //        Populate the fragment
-        args.let{
-            try{
+        args.let {
+            try {
                 Glide.with(requireActivity())
                     .load(it.dishDetails.image)
                     .centerCrop()
+//                        This listerner uses palette to get the background color based from the image passed in and apply it to the background of the fragment
+                    .listener(object : RequestListener<Drawable> {
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            Log.e("TAG", "Error loading image", e)
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            resource.let{
+//                                Get the value from palette and apply it to rlDishDetailMain
+                                Palette.from(resource!!.toBitmap()).generate() { palette ->
+                                    val intColor = palette?.vibrantSwatch?.rgb ?: palette?.dominantSwatch?.rgb ?: 0
+                                    mBinding!!.rlDishDetailMain.setBackgroundColor(intColor)
+                                }
+                            }
+                            return false
+                        }
+
+
+                    })
                     .into(mBinding!!.ivDishImage)
-            } catch (e: IOException){
+            } catch (e: IOException) {
                 e.printStackTrace()
             }
 
@@ -59,7 +97,8 @@ class DishDetailsFragment : Fragment() {
             mBinding!!.tvCategory.text = it.dishDetails.category
             mBinding!!.tvIngredients.text = it.dishDetails.ingredients
             mBinding!!.tvCookingDirection.text = it.dishDetails.directionsToCook
-            mBinding!!.tvCookingTime.text = resources.getString(R.string.lbl_estimate_cooking_time, it.dishDetails.cookingTime)
+            mBinding!!.tvCookingTime.text =
+                resources.getString(R.string.lbl_estimate_cooking_time, it.dishDetails.cookingTime)
 
 
         }
