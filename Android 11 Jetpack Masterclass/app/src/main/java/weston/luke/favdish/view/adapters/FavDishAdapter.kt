134a9842148e17ad79c48aa48876de7bf3bd6920
lkw19 <luke@weston.net.nz>
2023-02-09 00:28:01 +1300

    V34 added pop up menu on all dishes fragment to edit and delete an image

--- Android 11 Jetpack Masterclass/app/src/main/java/weston/luke/favdish/view/adapters/FavDishAdapter.kt
+++ Android 11 Jetpack Masterclass/app/src/main/java/weston/luke/favdish/view/adapters/FavDishAdapter.kt
@@ -1,23 +1,29 @@
 package weston.luke.favdish.view.adapters
 
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import weston.luke.favdish.R
 import weston.luke.favdish.databinding.ItemDishLayoutBinding
 import weston.luke.favdish.model.entities.FavDish
 import weston.luke.favdish.view.fragments.AllDishesFragment
 import weston.luke.favdish.view.fragments.FavoriteDishesFragment
 
-class FavDishAdapter(private val fragment: Fragment): RecyclerView.Adapter<FavDishAdapter.ViewHolder>() {
+class FavDishAdapter(private val fragment: Fragment) :
+    RecyclerView.Adapter<FavDishAdapter.ViewHolder>() {
 
     private var dishes: List<FavDish> = listOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 //        Create binding
         val binding: ItemDishLayoutBinding = ItemDishLayoutBinding.inflate(
-            LayoutInflater.from(fragment.context), parent, false)
+            LayoutInflater.from(fragment.context), parent, false
+        )
 //        Create the viewHolder specified in this class with the binding
         return ViewHolder(binding)
     }
@@ -30,13 +36,35 @@ class FavDishAdapter(private val fragment: Fragment): RecyclerView.Adapter<FavDi
             .into(holder.ivDishImage)
         holder.ivTitle.text = dish.title
 
-        holder.itemView.setOnClickListener{
-            if(fragment is AllDishesFragment){
+        holder.itemView.setOnClickListener {
+            if (fragment is AllDishesFragment) {
                 fragment.dishDetails(dish)
             }
-            if(fragment is FavoriteDishesFragment){
+            if (fragment is FavoriteDishesFragment) {
                 fragment.dishDetails(dish)
             }
+        }
+
+        holder.ibMore.setOnClickListener {
+            val popupMenu = PopupMenu(fragment.context, holder.ibMore)
+            popupMenu.menuInflater.inflate(R.menu.menu_adapter, popupMenu.menu)
+            popupMenu.setOnMenuItemClickListener {
+                if (it.itemId == R.id.action_edit_dish) {
+                    Log.i("you clicked", "edit dish")
+                } else if (it.itemId == R.id.action_delete_dish) {
+                    Log.i("you clicked", "delete dish")
+                }
+                true
+            }
+
+            popupMenu.show()
+        }
+// only show menu on all dishes fragment
+        if (fragment is AllDishesFragment) {
+            holder.ibMore.visibility = View.VISIBLE
+        }
+        else if(fragment is FavoriteDishesFragment){
+            holder.ibMore.visibility = View.GONE
         }
     }
 
@@ -44,14 +72,15 @@ class FavDishAdapter(private val fragment: Fragment): RecyclerView.Adapter<FavDi
         return dishes.size
     }
 
-//    Update the data with the observer in the fragment
-    fun dishesList(list: List<FavDish>){
+    //    Update the data with the observer in the fragment
+    fun dishesList(list: List<FavDish>) {
         dishes = list
         notifyDataSetChanged()
     }
 
-    class ViewHolder(view: ItemDishLayoutBinding) : RecyclerView.ViewHolder(view.root){
+    class ViewHolder(view: ItemDishLayoutBinding) : RecyclerView.ViewHolder(view.root) {
         val ivDishImage = view.ivDishImage
         val ivTitle = view.tvDishTitle
+        val ibMore = view.ibMore
     }
 }
