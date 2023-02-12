6aa5b996fe742ba62155676c288317bf59308fa4
lkw19 <luke@weston.net.nz>
2023-02-12 23:36:41 +1300

    Continued making the dog layout page

--- DogProfilePage/app/src/main/java/weston/luke/dogprofilepage/ProfilePage.kt
+++ DogProfilePage/app/src/main/java/weston/luke/dogprofilepage/ProfilePage.kt
@@ -1,27 +1,63 @@
 package weston.luke.dogprofilepage
 
 
+import android.provider.ContactsContract
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ProfilePage() {
-    Column {
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()) {
         Image(
-            painter = painterResource(id = R.drawable.snapchat_490394838),
-            contentDescription = "Holly"
+            painter = painterResource(id = R.drawable.holly),
+            contentDescription = "Holly",
+            modifier = Modifier
+                .size(150.dp)
+                .clip(shape = CircleShape)
+                .border(width = 2.dp, color = Color.Red, shape = CircleShape),
+            contentScale = ContentScale.Crop,
         )
-        Text(text="Holly dog")
-        Text(text="New Zealand")
+        Text(text = "Holly dog")
+        Text(text = "New Zealand")
+
+        Row(horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)) {
+            ProfileStats(count = "150", title = "Followers")
+            ProfileStats(count = "100", title = "Following")
+            ProfileStats(count = "20", title = "Posts")
+        }
     }
 }
 
+
+@Composable
+fun ProfileStats(count: String, title: String){
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = count, fontWeight = FontWeight.Bold)
+        Text(text = title)
+
+    }
+}
+
+
+
 @Preview(showBackground = true)
 @Composable
-fun ProfilePagePreview(){
+fun ProfilePagePreview() {
     ProfilePage()
 }
