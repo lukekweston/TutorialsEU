fd0bbbde1040fb6b659ce710247346d4be0f849b
lkw19 <luke@weston.net.nz>
2023-02-12 23:41:39 +1300

    Added two buttons and made the text for the name bold

--- DogProfilePage/app/src/main/java/weston/luke/dogprofilepage/ProfilePage.kt
+++ DogProfilePage/app/src/main/java/weston/luke/dogprofilepage/ProfilePage.kt
@@ -1,11 +1,13 @@
 package weston.luke.dogprofilepage
 
 
+import android.graphics.fonts.FontStyle
 import android.provider.ContactsContract
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -30,30 +32,46 @@ fun ProfilePage() {
                 .border(width = 2.dp, color = Color.Red, shape = CircleShape),
             contentScale = ContentScale.Crop,
         )
-        Text(text = "Holly dog")
+        Text(text = "Holly dog", fontWeight = FontWeight.Bold)
         Text(text = "New Zealand")
 
-        Row(horizontalArrangement = Arrangement.SpaceEvenly,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
             ProfileStats(count = "150", title = "Followers")
             ProfileStats(count = "100", title = "Following")
             ProfileStats(count = "20", title = "Posts")
         }
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Follow user")
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Direct message")
+            }
+        }
+
+
     }
 }
 
 
 @Composable
-fun ProfileStats(count: String, title: String){
+fun ProfileStats(count: String, title: String) {
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         Text(text = count, fontWeight = FontWeight.Bold)
         Text(text = title)
 
     }
 }
-
 
 
 @Preview(showBackground = true)
