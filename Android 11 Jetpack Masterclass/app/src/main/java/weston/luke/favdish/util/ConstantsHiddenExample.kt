3ba2ac8d1286341380212b3487b840c6426bda40
lkw19 <luke@weston.net.nz>
2023-02-13 00:15:43 +1300

    Fav Dish Created example constantsHidden file

---
+++ Android 11 Jetpack Masterclass/app/src/main/java/weston/luke/favdish/util/ConstantsHiddenExample.kt
@@ -0,0 +1,10 @@
+//Copy this file and rename it "ConstantsHidden"
+
+//get Api key from https://api.spoonacular.com/ you will have to make an account
+
+
+package weston.luke.favdish.util
+
+object ConstantsHiddenExample {
+    const val API_KEY_VALUE: String = ""
+}
