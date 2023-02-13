0e3dc8015323f782c2f04c5740c0f9999c2050e9
lkw19 <luke@weston.net.nz>
2023-02-13 23:04:38 +1300

    Started new composable Gmail clone app, created the Home App Bar

---
+++ GmailClone/app/src/main/java/weston/luke/gmailclone/MainActivity.kt
@@ -0,0 +1,48 @@
+package weston.luke.gmailclone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import weston.luke.gmailclone.components.HomeAppBar
+import weston.luke.gmailclone.ui.theme.GmailCloneTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GmailCloneTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    GmailApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GmailApp() {
+    Scaffold(topBar = { HomeAppBar() }) {
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    GmailCloneTheme {
+        GmailApp()
+    }
+}
